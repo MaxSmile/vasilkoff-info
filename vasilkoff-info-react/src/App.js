50277e47ae9dc92e7eb03711acdec39aabbe2b74
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2025-01-08 21:46:33 +0930

    chat

--- vasilkoff-info-react/src/App.js
+++ vasilkoff-info-react/src/App.js
@@ -1,14 +1,16 @@
 // src/App.js
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import RootPage from './pages/RootPage';
 import RedirectionPage from './pages/RedirectionPage';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<RootPage />} />
-      <Route path="/:slug" element={<RedirectionPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootPage />} />
+        <Route path="/:slug" element={<RedirectionPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
